351782b912a8ac00f305fa00d8c138f57686da70
Itiberê Gonçalves Silva Filho <56208580+Itibere10@users.noreply.github.com>
2022-08-24 21:24:06 -0300

    Delete run.sh

--- MAC Mapper/run.sh
+++
@@ -1,7 +0,0 @@
-echo "=================================================================="
-echo "Script de mapeamento e reconhecimento de endereços MAC"
-read -p "Digite a faixa de IPs para a busca (Ex: 10.0.0.1/24): " IPS
-#nmap -sP 10.0.0.1/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " | MAC:  "$3;}' | sort
-nmap -sP $IPS | awk '/Nmap scan report for/{ $5;}/MAC Address:/{print $3;}' | sort > macs.txt
-python search.py
-rm macs.txt
